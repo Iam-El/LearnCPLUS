d98e256b6e916f3c004eed160fa69b3af1a6bb93
Elsy Fernandes <elsyfernandes215@gmail.com>
2020-06-06 14:56:01 -0500

    commit2

--- Linker/C++ Linker/C++ Linker/Math.cpp
+++ Linker/C++ Linker/C++ Linker/Math.cpp
@@ -6,4 +6,22 @@
 //  Copyright © 2020 Iam_El. All rights reserved.
 //
 
+// this is the new math.cpp
+// it contain two functions but its not a main function it doesnt have main
+// If i click run on this file it will get compiled corrrectly . witgout no error
+
+// Now if i have ony this file without main file and try to build it we get an error (here linker comes into picture) - Link error 
+
 #include "Math.hpp"
+
+#include <iostream>  // 
+
+void log(const char* message){
+    std::cout<<message<<std::endl;
+}
+
+
+int Multiply(int a,int b){
+    log("Multiply");
+    return a*b;
+}
--- Linker/C++ Linker/C++ Linker/main.cpp
+++ Linker/C++ Linker/C++ Linker/main.cpp
@@ -7,9 +7,22 @@
 //
 
 #include <iostream>
+//#include "Math.cpp" //1) if you put only #include it wont work .since its not an header file
+// you should be using the decarations . now comment it out and put the declarations
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+void log(const char* message); //2 provide the declarations if its in different file
+int Multiply(int a,int b);
+
+void anotherPrint(const char* message){
+    std::cout<<message;
+}
+
+
+int main() {
+
     std::cout << "Hello, World!\n";
+    std::cout<<Multiply(5,10);
+    log("finished");
+//    anotherPrint("value");
     return 0;
 }
