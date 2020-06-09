4265281597f4f7a07f577724e554e53590c25883
Elsy Fernandes <elsyfernandes215@gmail.com>
2020-06-09 16:40:43 -0500

    commit15

--- static_insideclass/static2_insideclass/static2_insideclass/main.cpp
+++ static_insideclass/static2_insideclass/static2_insideclass/main.cpp
@@ -6,10 +6,28 @@
 //  Copyright © 2020 Iam_El. All rights reserved.
 //
 
+
+// this lecture on static methods cannot access non static variables
+
+// now lets make the member function as static then when we compile the code we will be gettig the error
+// because we cant access . you dont know which entity x and y instance you are trying to access.
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+struct Entity{
+    int x,y;
+    static void print(){
+        std::cout<<x<<' '<<y<<std::endl;
+        
+    }
+};
+int main() {
+    Entity e;
+    e.x=5;
+    e.y=10;
+    
+    Entity e1;
+    e1.x=15;
+    e1.y=20;
     return 0;
 }
